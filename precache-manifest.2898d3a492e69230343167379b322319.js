cc5c19ac795c72af9c5e5c900c7789a683662432
Brian Hoang <bhoang35@gmail.com>
2018-10-10 12:59:11 +0100

    Updates

---
+++ precache-manifest.2898d3a492e69230343167379b322319.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a98814c031d2c8aeed84",
+    "url": "/hacker-news-clone/static/js/runtime~main.a98814c0.js"
+  },
+  {
+    "revision": "71fccd1bf0414b1333f9",
+    "url": "/hacker-news-clone/static/js/main.71fccd1b.chunk.js"
+  },
+  {
+    "revision": "ce10d9bb7206231a2c49",
+    "url": "/hacker-news-clone/static/js/1.ce10d9bb.chunk.js"
+  },
+  {
+    "revision": "eed6255cc9a2a0c9175301e2c380f52d",
+    "url": "/hacker-news-clone/index.html"
+  }
+];
